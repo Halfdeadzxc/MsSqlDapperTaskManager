97ec3636d6a651e06d6f65c03f689ff78d3f9d90
dead <winter34577@gmail.com>
2025-10-08 14:21:15 +0300

    DAL + CLI

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿CREATE DATABASE TaskManager;
+GO
+
+USE TaskManager;
+GO
+
+CREATE TABLE Tasks (
+    Id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    Title NVARCHAR(255),
+    Description NVARCHAR(MAX),
+    IsCompleted BIT,
+    CreatedAt DATETIME DEFAULT GETDATE()
+);
+GO
